dac71a242c8b1d5fabf56ec375fc41b21c5e9513
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-07-03 23:12:52 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ int a[100],b[100],n,i;
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter n numbers");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ for(i=0;i<n;i++)
+ b[i]=a[i]*a[i];
+ printf("\n first array");
+ for(i=0;i<n;i++)
+ printf("%d\t",a[i]);
+ printf("\n square array");
+ for(i=0;i<n;i++)
+ printf("%d\t",b[i]);
+}
